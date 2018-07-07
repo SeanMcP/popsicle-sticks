ae28a12430c362819fc099225f1a833caebcde12
Sean McPherson <seanmcp25@gmail.com>
2018-07-07 13:52:52 -0400

    Create simple Header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../styles/index.css';
+import Header from './layout/Header';
 
 const App = ({ children }) => (
     <div className="app-container">
+        <Header/>
         {children}
     </div>
 );
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <header className="app-header">
+        Popsicle Sticks
+    </header>
+);
+
+export default Header;
