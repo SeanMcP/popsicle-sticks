957c3319e39a8a93b99b2cd84bfc24eccf710a24
Sean McPherson <seanmcp25@gmail.com>
2018-07-21 13:01:46 -0400

    Use PATH constant for routing endpoints

--- src/constants/path.js
+++ src/constants/path.js
@@ -1,4 +1,7 @@
 export const PATH = {
+    'group': '/section/:sectionId/group',
+    'random': '/section/:sectionId/random',
     'schedule': '/',
-    'section': '/section/:sectionId'
+    'section': '/section/:sectionId',
+    'student': '/student/:studentId'
 }
--- src/index.js
+++ src/index.js
@@ -17,12 +17,14 @@ import logger from 'redux-logger'
 import reducer from './reducers';
 
 import App from './components/App';
+import GroupContainer from './containers/GroupContainer';
 import RandomContainer from './containers/RandomContainer';
 import SectionContainer from './containers/SectionContainer';
 import ScheduleContainer from './containers/ScheduleContainer';
 import StudentContainer from './containers/StudentContainer';
 
 import './styles/_styles.css';
+import { PATH } from './constants';
 
 // Redux
 const history = createBrowserHistory();
@@ -41,10 +43,11 @@ ReactDOM.render(
             <ConnectedRouter history={history}>
                 <App>
                     <Switch>
-                        <Route path='/section/:sectionId/random' component={RandomContainer}/>
-                        <Route path='/section/:sectionId' component={SectionContainer}/>
-                        <Route path='/student/:studentId' component={StudentContainer}/>
-                        <Route exact path='/' component={ScheduleContainer}/>
+                        <Route path={PATH.group} component={GroupContainer}/>
+                        <Route path={PATH.random} component={RandomContainer}/>
+                        <Route path={PATH.section} component={SectionContainer}/>
+                        <Route path={PATH.student} component={StudentContainer}/>
+                        <Route exact path={PATH.schedule} component={ScheduleContainer}/>
                     </Switch>
                 </App>
             </ConnectedRouter>
