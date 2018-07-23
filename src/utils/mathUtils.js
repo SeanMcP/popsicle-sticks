d9123ff738e3c911d0a0b7d4fc6a0ccbee971888
Sean McPherson <seanmcp25@gmail.com>
2018-07-23 06:51:26 -0400

    Add MixedGroupsOf util

--- src/utils/mathUtils.js
+++ src/utils/mathUtils.js
@@ -13,10 +13,47 @@ export const Shuffle = (arr) => {
     return output;
 }
 
-export const FindLongestArr = (arrOfArrs) => {
+export const MixedGroupsOf = (size, arrOfArrs) => {
+    const output = [];
+    const longestLength = FindLongestArr(arrOfArrs, 'length');
+    let group = [];
+
+    for (let i = 0; i < longestLength; i++) {
+        // Iterate through a group
+
+        for (let g = 0; g < arrOfArrs.length; g++) {
+            // Interate through the groups
+            const current = arrOfArrs[g][i];
+
+            if (current) {
+                group.push(current);
+            }
+
+            if (group.length === size) {
+                output.push(group);
+                group = [];
+            }
+
+            if (
+                group.length &&
+                i === longestLength - 1 &&
+                g === arrOfArrs.length - 1
+            ) {
+                output.push(group);
+            }
+        }
+    }
+
+    return output;
+}
+
+export const FindLongestArr = (arrOfArrs, request) => {
+    // This intentionally does not care whether
+    // there are more than one longest array.
+
     let length = -Infinity;
     let index = -1;
-    
+
     arrOfArrs.forEach((arr, i) => {
         if (arr.length > length) {
             length = arr.length;
@@ -24,5 +61,11 @@ export const FindLongestArr = (arrOfArrs) => {
         }
     });
 
-    return index;
+    if (request === 'index') {
+        return index;
+    } else if (request === 'length') {
+        return arrOfArrs[index].length;
+    } else {
+        return arrOfArrs[index];
+    }
 }
