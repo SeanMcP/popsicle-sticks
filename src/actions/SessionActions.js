9f12f491cd68b2bceb1726a912d02e66230f81a9
Sean McPherson <seanmcp25@gmail.com>
2018-08-09 06:34:29 -0400

    Update create user action

--- src/actions/SessionActions.js
+++ src/actions/SessionActions.js
@@ -1,5 +1,8 @@
 // SessionActions.js
+import { push } from 'connected-react-router';
 import { auth } from '../firebase';
+import { setNotification } from '../actions';
+import { PATH } from '../constants';
 
 export const SESSION_ACTIONS = {
     AUTHENTICATE: 'AUTHENTICATE',
@@ -18,8 +21,21 @@ export const authenticateUser = (user) => {
     }
 }
 
-export const userCreateWithEmailAndPassword = (email, password) =>
-    auth.createUserWithEmailAndPassword(email, password);
+export const userCreateWithEmailAndPassword = (email, password) => {
+    return (dispatch) => {
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(() => {
+                dispatch(push(PATH.schedule));
+            })
+            .catch(error => {
+                dispatch(setNotification({
+                    type: 'FAILURE',
+                    message: error.message,
+                    error
+                }));
+            });
+    }
+}
 
 export const userPasswordReset = (email) =>
     auth.sendPasswordResetEmail(email);
